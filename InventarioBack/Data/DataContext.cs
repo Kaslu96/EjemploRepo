3273a4c95c5338269ffa2d71cd9855d8f9523eac
salak406 <salak406@SALAK406-35>
2024-11-12 07:25:35 -0500

    Agregar archivos de proyecto.

---
+++ InventarioBack/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿namespace InventarioBack.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class DataContext
+    {
+        public DataContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
